be5f1aa650ab6c756f60bd425d020f2c93554b44
Alessandro Viganò <alvistar@gmail.com>
2024-12-19 19:15:09 +0100

    Added metrics

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.21
 
 require (
 	github.com/fluxcd/image-reflector-controller/api v0.31.1
+	github.com/prometheus/client_golang v1.18.0
 	k8s.io/api v0.29.0
 	k8s.io/apimachinery v0.29.0
 	k8s.io/client-go v0.29.0
@@ -11,6 +12,8 @@ require (
 )
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
 	github.com/evanphx/json-patch/v5 v5.8.0 // indirect
@@ -29,10 +32,14 @@ require (
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/matttproud/golang_protobuf_extensions/v2 v2.0.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.45.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/oauth2 v0.12.0 // indirect
--- main.go
+++ main.go
@@ -4,6 +4,11 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"path/filepath"
 	"strings"
 
@@ -26,9 +31,83 @@ func getBaseImage(image string) string {
 	return image
 }
 
+var (
+	updateAvailable = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "update_available",
+			Help: "Indicates if a newer version is available (1) or not (0)",
+		},
+		[]string{"namespace", "pod", "container"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(updateAvailable)
+}
+
+func checkUpdates(c client.Client) {
+	// List ImagePolicies from all namespaces
+	var imagePolicies imagev1.ImagePolicyList
+	if err := c.List(context.Background(), &imagePolicies); err != nil {
+		fmt.Printf("Error listing ImagePolicies: %v\n", err)
+		return
+	}
+
+	// List all pods
+	var pods corev1.PodList
+	if err := c.List(context.Background(), &pods); err != nil {
+		fmt.Printf("Error listing Pods: %v\n", err)
+		return
+	}
+
+	// Check ImagePolicies and pods
+	fmt.Printf("\nFound %d ImagePolicies:\n", len(imagePolicies.Items))
+	for _, policy := range imagePolicies.Items {
+		fmt.Printf("\nName: %s\n", policy.Name)
+		fmt.Printf("Namespace: %s\n", policy.Namespace)
+		
+		latestImage := policy.Status.LatestImage
+		if latestImage != "" {
+			fmt.Printf("Latest Image: %s\n", latestImage)
+			baseLatestImage := getBaseImage(latestImage)
+
+			// Check pods using this image
+			for _, pod := range pods.Items {
+				for _, container := range pod.Spec.Containers {
+					if getBaseImage(container.Image) == baseLatestImage {
+						fmt.Printf("Pod %s/%s using image: %s\n", 
+							pod.Namespace, 
+							pod.Name, 
+							container.Image)
+						
+						// Update metric
+						value := 0.0
+						if container.Image != latestImage {
+							fmt.Printf("  -> Not using latest version!\n")
+							value = 1.0
+						}
+						updateAvailable.With(prometheus.Labels{
+							"namespace": pod.Namespace,
+							"pod":      pod.Name,
+							"container": container.Name,
+						}).Set(value)
+					}
+				}
+			}
+		}
+		
+		if policy.Spec.Policy.SemVer != nil {
+			fmt.Printf("Semver Range: %s\n", policy.Spec.Policy.SemVer.Range)
+		}
+	}
+}
+
 func main() {
-	// Setup kubernetes config
-	var kubeconfig *string
+	var (
+		kubeconfig *string
+		interval   = flag.Duration("interval", 5*time.Minute, "Polling interval")
+		addr      = flag.String("listen-address", ":2112", "The address to listen on for HTTP requests.")
+	)
 	if home := homedir.HomeDir(); home != "" {
 		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
 	} else {
@@ -64,35 +143,25 @@ func main() {
 		panic(err.Error())
 	}
 
-	// Print ImagePolicies and check pods
-	fmt.Printf("\nFound %d ImagePolicies:\n", len(imagePolicies.Items))
-	for _, policy := range imagePolicies.Items {
-		fmt.Printf("\nName: %s\n", policy.Name)
-		fmt.Printf("Namespace: %s\n", policy.Namespace)
-		
-		latestImage := policy.Status.LatestImage
-		if latestImage != "" {
-			fmt.Printf("Latest Image: %s\n", latestImage)
-			baseLatestImage := getBaseImage(latestImage)
+	// Start initial check
+	checkUpdates(c)
 
-			// Check pods using this image
-			for _, pod := range pods.Items {
-				for _, container := range pod.Spec.Containers {
-					if getBaseImage(container.Image) == baseLatestImage {
-						fmt.Printf("Pod %s/%s using image: %s\n", 
-							pod.Namespace, 
-							pod.Name, 
-							container.Image)
-						if container.Image != latestImage {
-							fmt.Printf("  -> Not using latest version!\n")
-						}
-					}
-				}
-			}
+	// Setup metrics endpoint
+	http.Handle("/metrics", promhttp.Handler())
+
+	// Start polling routine
+	go func() {
+		ticker := time.NewTicker(*interval)
+		defer ticker.Stop()
+
+		for range ticker.C {
+			checkUpdates(c)
 		}
-		
-		if policy.Spec.Policy.SemVer != nil {
-			fmt.Printf("Semver Range: %s\n", policy.Spec.Policy.SemVer.Range)
-		}
+	}()
+
+	// Start HTTP server
+	fmt.Printf("Starting metrics server on %s\n", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		panic(err)
 	}
 }
