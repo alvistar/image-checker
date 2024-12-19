88800b69aa2c3cbe27404ea2bcdeb5844f6c0ae1
Alessandro Viganò <alvistar@gmail.com>
2024-12-19 19:06:02 +0100

    Checking pod version

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.21
 
 require (
 	github.com/fluxcd/image-reflector-controller/api v0.31.1
+	k8s.io/api v0.29.0
 	k8s.io/apimachinery v0.29.0
 	k8s.io/client-go v0.29.0
 	sigs.k8s.io/controller-runtime v0.17.0
@@ -44,7 +45,6 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/api v0.29.0 // indirect
 	k8s.io/klog/v2 v2.110.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20231010175941-2dd684a91f00 // indirect
 	k8s.io/utils v0.0.0-20231127182322-b307cd553661 // indirect
--- main.go
+++ main.go
@@ -5,13 +5,26 @@ import (
 	"flag"
 	"fmt"
 	"path/filepath"
+	"strings"
 
 	imagev1 "github.com/fluxcd/image-reflector-controller/api/v1beta2"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 	runtime "k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
+
+func getBaseImage(image string) string {
+	// Remove tag or digest
+	if strings.Contains(image, "@sha256:") {
+		return strings.Split(image, "@sha256:")[0]
+	}
+	if strings.Contains(image, ":") {
+		return strings.Split(image, ":")[0]
+	}
+	return image
+}
 
 func main() {
 	// Setup kubernetes config
@@ -32,6 +45,7 @@ func main() {
 	// Create controller-runtime client
 	scheme := runtime.NewScheme()
 	imagev1.AddToScheme(scheme)
+	corev1.AddToScheme(scheme)
 	
 	c, err := client.New(config, client.Options{Scheme: scheme})
 	if err != nil {
@@ -44,14 +58,39 @@ func main() {
 		panic(err.Error())
 	}
 
-	// Print ImagePolicies
+	// List all pods
+	var pods corev1.PodList
+	if err := c.List(context.Background(), &pods); err != nil {
+		panic(err.Error())
+	}
+
+	// Print ImagePolicies and check pods
 	fmt.Printf("\nFound %d ImagePolicies:\n", len(imagePolicies.Items))
 	for _, policy := range imagePolicies.Items {
 		fmt.Printf("\nName: %s\n", policy.Name)
 		fmt.Printf("Namespace: %s\n", policy.Namespace)
-		if policy.Status.LatestImage != "" {
-			fmt.Printf("Latest Image: %s\n", policy.Status.LatestImage)
+		
+		latestImage := policy.Status.LatestImage
+		if latestImage != "" {
+			fmt.Printf("Latest Image: %s\n", latestImage)
+			baseLatestImage := getBaseImage(latestImage)
+
+			// Check pods using this image
+			for _, pod := range pods.Items {
+				for _, container := range pod.Spec.Containers {
+					if getBaseImage(container.Image) == baseLatestImage {
+						fmt.Printf("Pod %s/%s using image: %s\n", 
+							pod.Namespace, 
+							pod.Name, 
+							container.Image)
+						if container.Image != latestImage {
+							fmt.Printf("  -> Not using latest version!\n")
+						}
+					}
+				}
+			}
 		}
+		
 		if policy.Spec.Policy.SemVer != nil {
 			fmt.Printf("Semver Range: %s\n", policy.Spec.Policy.SemVer.Range)
 		}
